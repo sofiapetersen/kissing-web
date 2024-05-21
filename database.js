8f7005a8f74c9b3c7b16e7cb4557bf02dc973aa1
Sofia Petersen <sppessil@inf.ufpel.edu.br>
2024-05-21 17:13:16 -0300

    falta deixar bonito

---
+++ database.js
@@ -0,0 +1,12 @@
+// database.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'kissingweb', 
+  password: '1234',
+  port: 5432,
+});
+
+module.exports = pool;
