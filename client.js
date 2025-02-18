292be8afd715d9002fa17d3ecd0d283b675adc77
Sofia Petersen <sppessil@inf.ufpel.edu.br>
2025-02-18 12:12:04 -0300

    db atualizado

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-    "https://pygqirmwmklcmhbgflgp.supabase.co",
+    "https://kpjckkhyqplqefllxuio.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwamNra2h5cXBscWVmbGx4dWlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4OTEwNjYsImV4cCI6MjA1NTQ2NzA2Nn0.y0qYT1dz2WATlBXOWONJs6zhDN8Ch2vomMZ6nhqb8-o"
 );
 
