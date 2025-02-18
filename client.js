5c1beaa1652691551b4010dfc522e37a8ca515af
Sofia Petersen <sppessil@inf.ufpel.edu.br>
2025-02-18 12:10:11 -0300

    troquei o db

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
     "https://pygqirmwmklcmhbgflgp.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5Z3Fpcm13bWtsY21oYmdmbGdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0ODU2NzcsImV4cCI6MjAzMjA2MTY3N30.XwgiRamAuySwqx6EvvWam8i9_EvG4aW2PUTEXeMZWfQ"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwamNra2h5cXBscWVmbGx4dWlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4OTEwNjYsImV4cCI6MjA1NTQ2NzA2Nn0.y0qYT1dz2WATlBXOWONJs6zhDN8Ch2vomMZ6nhqb8-o"
 );
 
 module.exports = supabase;
